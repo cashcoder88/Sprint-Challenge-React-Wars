68b4f35197ccf6330c567722b257abea61f085a0
cashcoder88 <cashglobe@gmail.com>
2019-04-26 09:17:22 -0600

    testing map, getting names

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -32,7 +32,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">React Wars Baby!</h1>
+        <div>
+          {this.state.starwarsChars.map((char) => (
+            <div>
+              <h1>{char.name}</h1>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
